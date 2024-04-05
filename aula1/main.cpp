8b39c6765e7735aba7307965e1bec8a835116d71
KingTimer12 <queentimer12@gmail.com>
2024-04-04 23:01:50 -0300

    feat: init;

---
+++ aula1/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// & = Endereçamento
+// &<var> = Referência
+// *<var> = Ponteiro
+
+// ptr => Ponteiro
+// ref => Referência
+// const => Constância
+
+void mean(float *scores, const int size, float &result)
+{
+    for (int i = 0; i < size; i++)
+        result = scores[i] + result;
+    result = result / size;
+}
+
+int main()
+{
+    string name;
+    int size;
+
+    float *ptr_scores = nullptr;
+
+    float result;
+
+    cout << "Escreva seu nome: ";
+    getline(cin, name);
+    cout << "Quantas provas foram feitas?\n";
+    cin >> size;
+    ptr_scores = new float[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Qual foi a nota da prova "<< i+1 <<"?\n";
+        cin >> ptr_scores[i];
+    }
+
+    mean(ptr_scores, size, result);
+    cout << "Sua media foi: " << result << "\n";
+
+    delete[] ptr_scores;
+
+    return 0;
+}
